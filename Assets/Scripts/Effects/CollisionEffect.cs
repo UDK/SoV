b2e6f5204a78e7859cce7fee41de62c3ce100b4f
vladisl <->
2020-05-04 05:01:53 +0400

    fix

--- Assets/Scripts/Effects/CollisionEffect.cs
+++ Assets/Scripts/Effects/CollisionEffect.cs
@@ -5,20 +5,26 @@ using UnityEngine.VFX;
 
 public class CollisionEffect : MonoBehaviour
 {
-    [SerializeField]
-    public VisualEffect visualEffect;
+    public VisualEffect collisionEffect;
+    public VisualEffect collisionAfterEffect;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Vector3 normal = collision.contacts[0].normal;
         var angle = Vector3.SignedAngle(-normal, Vector3.right, -Vector3.forward);
         var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        var ripples = Instantiate(
-            visualEffect,
-            new Vector3(transform.position.x, transform.position.y, visualEffect.transform.position.z),
+        var position = new Vector3(transform.position.x, transform.position.y, collisionEffect.transform.position.z);
+        var collisionE = Instantiate(
+            collisionEffect,
+            position,
             rotation,
             transform);
-
-        Destroy(ripples.gameObject, 5);
+        /*var collisionAfterE = Instantiate(
+            collisionAfterEffect,
+            position,
+            rotation,
+            transform.parent);*/
+        Destroy(collisionE.gameObject, 5);
+        /*Destroy(collisionAfterE.gameObject, 5);*/
     }
 }
