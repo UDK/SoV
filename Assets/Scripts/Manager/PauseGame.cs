d0030354ae69f2fb440fc89f0073126073ac7160
UDK <udk1@bk.ru>
2020-06-02 18:07:33 +0300

    Добавил скрипт на паузу

---
+++ Assets/Scripts/Manager/PauseGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    private bool gameIsPause = false;
+    public bool GameIsPause {
+        get => gameIsPause;
+        set
+        {
+            if(value == true)
+            {
+                Time.timeScale = 0;
+                gameIsPause = value;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                gameIsPause = value;
+            }
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Esc всегда escape и переопределения, вряд ли, будет
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameIsPause = !GameIsPause;
+        }
+    }
+}
