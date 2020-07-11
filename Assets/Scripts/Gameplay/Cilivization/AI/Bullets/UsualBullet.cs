51e52b36fbde3776f01855d76a68e4e3ea66cb89
vladisl <->
2020-07-11 23:40:30 +0400

    prepare for more fun

--- Assets/Scripts/Gameplay/Cilivization/AI/Bullets/UsualBullet.cs
+++ Assets/Scripts/Gameplay/Cilivization/AI/Bullets/UsualBullet.cs
@@ -11,7 +11,7 @@ namespace Assets.Scripts.Gameplay.Cilivization.AI.Bullets
 {
     public class UsualBullet : MonoBehaviour, IGameplayObject, IShell
     {
-        public float LifeDistance { get; set; } = 4f;
+        public float LifeDistance { get; set; } = 10f;
 
         public GameObject Target { get; set; }
 
@@ -19,12 +19,15 @@ namespace Assets.Scripts.Gameplay.Cilivization.AI.Bullets
 
         private MovementBehaviour _movementBehaviour;
 
+        private Vector3 _startPosition { get; set; }
+
         public UsualBullet()
         {
         }
 
         public void Initiate()
         {
+            _startPosition = transform.position;
             _movementBehaviour = GetComponent<MovementBehaviour>();
             var moveVector = Target.transform.position - transform.position;
             _movementBehaviour.SetVelocity(
@@ -33,6 +36,14 @@ namespace Assets.Scripts.Gameplay.Cilivization.AI.Bullets
             float angle = Mathf.Atan2(_movementBehaviour.Velocity.y,
                 _movementBehaviour.Velocity.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        private void Update()
+        {
+            if(Vector3.Distance(transform.position, _startPosition) > LifeDistance)
+            {
+                Destroy(gameObject);
+            }
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
